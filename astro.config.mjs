e1d0ffc393560e11de74d276ee2b6ae3a5b3444f
diguillindev <scontreras.dev@gmail.com>
2025-05-12 12:13:50 -0400

    -preparacion de desarrollo a deploy -cambio de metodo para agregar imagenes {`${import.meta.env.BASE_URL}

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+// configuration deploy github
+  site: 'https://diguillindev.github.io/diguillin-dev-portafolio/',
+  base: '/diguillin-dev-portafolio/', 
   vite: {
     plugins: [tailwindcss()]
   }
